6a6289da4ec502c3ba9459d2729270acc60a08a1
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-10-21 22:34:36 +0300

    tmp

--- routes/api/propertiesRouter.js
+++ routes/api/propertiesRouter.js
@@ -6,7 +6,6 @@ import {
   authenticate,
   isBodyEmpty,
   isValidId,
-  // upload,
 } from '../../middleware/index.js';
 
 // ####################################################
@@ -18,31 +17,5 @@ propertiesRouter.use(authenticate); // middleware to be executed for all routes
 propertiesRouter.get('/', controller.getAll);
 
 propertiesRouter.get('/:id', isValidId, controller.getById);
-
-propertiesRouter.post(
-  '/',
-  // upload.single('photo'),
-  isBodyEmpty,
-  validateBody(schemas.propertiesAddSchema),
-  controller.add
-);
-
-propertiesRouter.delete('/:id', controller.deleteById);
-
-propertiesRouter.put(
-  '/:id',
-  isValidId,
-  isBodyEmpty,
-  validateBody(schemas.propertiesAddSchema),
-  controller.updateById
-);
-
-propertiesRouter.patch(
-  '/:id/favorite',
-  isValidId,
-  isBodyEmpty,
-  validateBody(schemas.propertiesToggleFavoriteSchema),
-  controller.updateStatusProperty
-);
 
 export default propertiesRouter;
